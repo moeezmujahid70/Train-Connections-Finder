2a596becfcda1c615f4e15247330b76a07e3493f
Moeez Mujahid <moeez.mujahid@fau.de>
2024-11-09 20:33:35 +0100

    main script

---
+++ main.py
@@ -0,0 +1,22 @@
+from solver.problem_solver import load_problems_csv, read_and_preprocess_csv
+from solver.graph_builder import build_graph
+
+
+def main():
+
+    problems_file = "problems/example-problems.csv"
+    schedule_file = "data/mini-schedule.csv"  # File containing schedule data
+
+    # Load and preprocess the problem
+    problems_df = load_problems_csv(problems_file)
+    schedule_df = read_and_preprocess_csv(schedule_file)
+    print(schedule_df.head())
+    print(problems_df.head())
+
+    # build Graph from data
+    graph = build_graph(schedule_df)
+    print(graph)
+
+
+if __name__ == "__main__":
+    main()
