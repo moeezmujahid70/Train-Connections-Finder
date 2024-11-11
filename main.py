f931ad0a0340fc0ecb6fa6a24741290fe498f764
Sandipan Nath <sandipannath123@gmail.com>
2024-11-11 22:19:46 +0100

    wrote stop_cost mini-schedule solutions to csv

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from solver.problem_solver import load_problems_csv, read_and_preprocess_csv, solve_cost_stop
+from solver.problem_solver import load_problems_csv, read_and_preprocess_csv, solve_cost_stop, create_solutions_csv
 from solver.graph_builder import build_graph
+import pandas as pd
 
 def main():
-
+    
     problems_file = "problems/example-problems.csv"
     schedule_file = "data/mini-schedule.csv"  # File containing schedule data
 
@@ -11,10 +12,19 @@ def main():
     schedule_df = read_and_preprocess_csv(schedule_file)
     print(schedule_df.head())
     print(problems_df.head())
-
     # build Graph from data
     graph = build_graph(schedule_df)
-    print(solve_cost_stop(graph, 'TK', 'SGD', schedule_df))
+    rslt_df = problems_df.loc[(problems_df['CostFunction'] == 'stops') & (problems_df['Schedule'] == 'mini-schedule.csv')]
+    solutions = {'ProblemNo':[],'Connection':[],'Cost':[]}
+    for _, row in rslt_df.iterrows():
+        connection, cost = solve_cost_stop(graph, row['FromStation'], row['ToStation'], schedule_df)
+        solutions['ProblemNo'].append(row['ProblemNo'])
+        solutions['Connection'].append(connection)
+        solutions['Cost'].append(cost)
+    create_solutions_csv(solutions, 'solutions/my-example-solutions.csv')
+    
+    
+    
         
     
 
