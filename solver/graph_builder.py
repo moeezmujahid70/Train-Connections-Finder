f0a0d9d21f83448083cfb4f765b65b10dc5d9055
Moeez Mujahid <moeez.mujahid@fau.de>
2024-11-13 00:46:08 +0100

    implemented function to calculate travel time between nodes

--- solver/graph_builder.py
+++ solver/graph_builder.py
@@ -1,23 +1,51 @@
+from datetime import datetime, timedelta
 import pandas as pd
 import networkx as nx
 
 
+# Note: might need to implement threading for later CostFuntions Graphs
+
+def calculate_travel_time(from_station, to_station) -> timedelta:
+
+    # Parse departure and arrival times
+    departure_time = datetime.strptime(
+        from_station['Departure time'].strip("'"), "%H:%M:%S")
+    arrival_time = datetime.strptime(
+        to_station['Arrival time'].strip("'"), "%H:%M:%S")
+
+    # Handle overnight travel where arrival time is on the next day
+    if arrival_time < departure_time:
+        arrival_time += timedelta(days=1)
+
+    return (arrival_time - departure_time)
+
+
 def build_graph(schedule_df: pd.DataFrame) -> nx.DiGraph:
     """
-    Builds a directed graph from the schedule data using NetworkX.
+    Builds a directed graph from the schedule data using NetworkX, adding timeintrain.
     """
     G = nx.DiGraph()  # Directed graph
 
     # Group by train number to handle consecutive stops for each train
     for train_no, group in schedule_df.groupby('Train No.'):
-        group = group.sort_values('islno')
+        group = group.sort_values('islno')  # Sort by stop order (islno)
 
         # Iterate through consecutive stops for each train
         for i in range(len(group) - 1):
             from_station = group.iloc[i]['station Code']
             to_station = group.iloc[i + 1]['station Code']
 
-            # Add a directed edge from from_station to to_station
-            G.add_edge(from_station, to_station, train=train_no, weight = 1)
+            # Calculate travel time in seconds
+            travel_time_seconds = calculate_travel_time(
+                group.iloc[i],  group.iloc[i + 1]).seconds
+
+            # Add a directed edge from from_station to to_station with default weight and timeintrain
+            G.add_edge(
+                from_station,
+                to_station,
+                train=train_no,
+                weight=1,  # Default weight (costFuntion:Stops)
+                timeintrain=travel_time_seconds  # Add time spent in the train
+            )
 
     return G
