14f2d80727f71ba3ef94b63a96482f30c7e6354a
Moeez Mujahid <moeez.mujahid@fau.de>
2024-11-17 23:27:44 +0100

    added cost funtion arrival time

--- solver/costFuntions.py
+++ solver/costFuntions.py
@@ -1,23 +1,36 @@
 import heapq
 from solver.problem_solver import construct_connection, construct_connection_expanded_graph
+from solver.utils import add_seconds_to_time
 
 
-def solve_cost_funtion(graph, start, target, schedule_df, cost_attribute='stops'):
+def solve_cost_funtion(graph, start, target, schedule_df, cost_attribute='stops', input_arrival_time='00:00:00'):
     """
     Solves for the optimal path based on the specified cost attribute and formats the solution.
     """
-    if cost_attribute =='stops' or cost_attribute =='timeintrain':
+    if cost_attribute == 'stops' or cost_attribute == 'timeintrain':
         station_sequence, train_sequence, total_cost = dijkstra_path(
             graph, start, target, cost_attribute)
         if station_sequence is None:
             return 'PATH NOT FOUND'
-        connection = construct_connection(schedule_df, station_sequence, train_sequence)
-    else:
-        node_sequence, total_cost = dijkstra_path_expanded_graph(graph, start, target)
+        connection = construct_connection(
+            schedule_df, station_sequence, train_sequence)
+    elif cost_attribute == 'traveltime':
+        node_sequence, total_cost = dijkstra_path_expanded_graph(
+            graph, start, target)
         if node_sequence is None:
             return 'PATH NOT FOUND'
-        #print(node_sequence)
         connection = construct_connection_expanded_graph(node_sequence)
+    elif cost_attribute == 'arrivaltime':
+        node_sequence, total_cost = dijkstra_path_expanded_graph(
+            graph, start, target)
+        if node_sequence is None:
+            return 'PATH NOT FOUND'
+        connection = construct_connection_expanded_graph(node_sequence)
+        total_cost = add_seconds_to_time(input_arrival_time, total_cost)
+
+    else:
+        return 'Invalid Cost Attribute'
+
     return connection, str(total_cost)
 
 
@@ -83,6 +96,7 @@ def dijkstra_path(graph, start, target, cost_attribute):
     # Return if target is unreachable
     return None, None, None
 
+
 def dijkstra_path_expanded_graph(graph, start, target):
     """
     Dijkstra's algorithm to find the shortest path in a MultiDiGraph.
@@ -119,29 +133,25 @@ def dijkstra_path_expanded_graph(graph, start, target):
 
         # Check all edges to neighbors of the current node
         for neighbor, attribute in graph[current_node].items():
-                travel_cost = current_cost + attribute['time']
+            travel_cost = current_cost + attribute['time']
 
-                # Only update if this path is shorter
-                if travel_cost < costs[neighbor]:
-                    costs[neighbor] = travel_cost
-                    predecessors[neighbor] = current_node
-                    costs_from_pred[neighbor] = attribute['time']
-                    heapq.heappush(priority_queue, (travel_cost, neighbor))
+            # Only update if this path is shorter
+            if travel_cost < costs[neighbor]:
+                costs[neighbor] = travel_cost
+                predecessors[neighbor] = current_node
+                costs_from_pred[neighbor] = attribute['time']
+                heapq.heappush(priority_queue, (travel_cost, neighbor))
 
     # If we reach the target node, reconstruct the path
     if predecessors[target] is not None:
         # Reconstruct the optimal path
         current_node = target
-        node_sequence=[]
-        #cost_sequence=[]
+        node_sequence = []
         while current_node is not None:
             node_sequence.append(current_node)
             current_node = predecessors[current_node]
-            #print(current_node)
-            #if current_node is not None and costs_from_pred[current_node] is not None:
-            #    cost_sequence.append(costs_from_pred[current_node])
+
         return node_sequence[::-1], costs[target]
-        #return node_sequence[::-1], cost_sequence[::-1], costs[target]
 
     # Return if target is unreachable
     return None, None
