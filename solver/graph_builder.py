abc5261c144325395de3fe17a39676978a6f115e
Moeez Mujahid <moeez.mujahid@fau.de>
2024-11-17 18:11:43 +0100

    code cleanup

--- solver/graph_builder.py
+++ solver/graph_builder.py
@@ -1,35 +1,7 @@
-from datetime import datetime, timedelta
+from collections import defaultdict
+from solver.utils import calculate_time_differenc
 import pandas as pd
 import networkx as nx
-
-
-# Note: might need to implement threading for later CostFuntions Graphs
-
-def calculate_travel_time(from_station, to_station) -> timedelta:
-
-    # Parse departure and arrival times
-    departure_time = datetime.strptime(
-        from_station['Departure time'].strip("'"), "%H:%M:%S")
-    arrival_time = datetime.strptime(
-        to_station['Arrival time'].strip("'"), "%H:%M:%S")
-
-    # Handle overnight travel where arrival time is on the next day
-    if arrival_time < departure_time:
-        arrival_time += timedelta(days=1)
-
-    return (arrival_time - departure_time)
-
-def calculate_time(from_time, to_time):
-    from_time = datetime.strptime(
-        from_time.strip("'"), "%H:%M:%S")
-    to_time = datetime.strptime(
-        to_time.strip("'"), "%H:%M:%S")
-
-    # Handle overnight travel where arrival time is on the next day
-    if to_time < from_time:
-        to_time += timedelta(days=1)
-        
-    return (to_time - from_time)
 
 
 def build_graph(schedule_df: pd.DataFrame) -> nx.MultiDiGraph:
@@ -51,8 +23,8 @@ def build_graph(schedule_df: pd.DataFrame) -> nx.MultiDiGraph:
             from_islno = group.iloc[i]['islno']
             to_islno = group.iloc[i+1]['islno']
             # Calculate travel time in seconds
-            travel_time_seconds = calculate_travel_time(
-                group.iloc[i], group.iloc[i + 1]).seconds
+            travel_time_seconds = calculate_time_differenc(
+                group.iloc[i]['Departure time'], group.iloc[i + 1]['Arrival time']).seconds
 
             # Add a directed edge with train-specific attributes
             G.add_edge(
@@ -69,24 +41,60 @@ def build_graph(schedule_df: pd.DataFrame) -> nx.MultiDiGraph:
 
     return G
 
+
 def expand_graph(graph: nx.MultiDiGraph) -> nx.DiGraph:
-    
-    arr_node_dict = {node: {} for node in graph}
-    dep_node_dict = {node: {} for node in graph}
+    """
+    Expands a MultiDiGraph to capture detailed arrival and departure times at stations.
+    """
     expanded_graph = nx.DiGraph()
-    
+    arr_node_dict = defaultdict(dict)
+    dep_node_dict = defaultdict(dict)
+
+    # Add edges between arrival and departure nodes
     for node in graph:
         for in_edge in graph.in_edges(node, data=True):
-            expanded_graph.add_edge((in_edge[0], in_edge[2]['train'], in_edge[2]['fromislno'], 'dep'), (node,  in_edge[2]['train'], in_edge[2]['toislno'], 'arr'), time=in_edge[2]['timeintrain'])
-            arr_node_dict[node][(node,  in_edge[2]['train'], in_edge[2]['toislno'], 'arr')] = in_edge[2]['arrivaltime']
-            dep_node_dict[in_edge[0]][(in_edge[0], in_edge[2]['train'], in_edge[2]['fromislno'], 'dep')] = in_edge[2]['departuretime']
-    
-    for node in graph:
-        for dep_node in dep_node_dict[node].keys():
-            expanded_graph.add_edge((node, '0', -1, 'start'), dep_node, time=0)
-            for arr_node in arr_node_dict[node].keys():
-                expanded_graph.add_edge(arr_node, dep_node, time = calculate_time(arr_node_dict[node][arr_node], dep_node_dict[node][dep_node]).seconds)
-        for arr_node in arr_node_dict[node].keys():
+            dep_node = (in_edge[0], in_edge[2]['train'],
+                        in_edge[2]['fromislno'], 'dep')
+            arr_node = (node, in_edge[2]['train'],
+                        in_edge[2]['toislno'], 'arr')
+
+            # Add edge to expanded graph with departure time as an attribute
+            expanded_graph.add_edge(
+                dep_node,
+                arr_node,
+                time=in_edge[2]['timeintrain'],
+                departuretime=in_edge[2]['departuretime']
+            )
+
+            # Store arrival and departure times in dictionaries
+            arr_node_dict[node][arr_node] = in_edge[2]['arrivaltime']
+            dep_node_dict[in_edge[0]][dep_node] = in_edge[2]['departuretime']
+
+    # Ensure every station has virtual start and end connections
+    all_nodes = set(graph.nodes())
+
+    for node in all_nodes:
+        dep_nodes = dep_node_dict.get(node, {})
+        arr_nodes = arr_node_dict.get(node, {})
+
+        # Connect virtual start to all departure nodes
+        for dep_node, dep_time in dep_nodes.items():
+            expanded_graph.add_edge(
+                (node, '0', -1, 'start'),
+                dep_node,
+                time=0,
+                departuretime=dep_time
+            )
+
+        # Connect all arrival nodes to virtual end
+        for arr_node in arr_nodes:
             expanded_graph.add_edge(arr_node, (node, '0', -1, 'end'), time=0)
-            
+
+        # Connect all arrival nodes to departure nodes within the same station
+        for arr_node, arr_time in arr_nodes.items():
+            for dep_node, dep_time in dep_nodes.items():
+                time_spent = calculate_time_differenc(
+                    arr_time, dep_time).seconds
+                expanded_graph.add_edge(arr_node, dep_node, time=time_spent)
+
     return expanded_graph
