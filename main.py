a7e5c19820be58fc3871c74a236dcb2aa62821cd
Moeez Mujahid <moeez.mujahid@fau.de>
2024-11-17 23:26:44 +0100

    optimized for all problems input

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from solver.problem_solver import load_problems_csv, read_and_preprocess_csv, create_solutions_csv
 from solver.costFuntions import solve_cost_funtion
-from solver.graph_builder import build_graph, expand_graph
-import pickle
+from solver.graph_builder import build_graph, expand_graph, adjust_start_times
+
 
 def main():
 
@@ -19,52 +19,43 @@ def main():
     mini_graph = build_graph(mini_schedule_df)
     expanded_mini_graph = expand_graph(mini_graph)
     expanded_graph = expand_graph(graph)
-    
-   # Display some edges with the new timeintrain attribute
 
-    rslt_df = problems_df.loc[(problems_df['CostFunction'] == 'stops')]
-    rslt_df_2 = problems_df.loc[(problems_df['CostFunction'] == 'timeintrain')]
-    rslt_df_4 = problems_df.loc[(problems_df['CostFunction'] == 'traveltime')]
     solutions = {'ProblemNo': [], 'Connection': [], 'Cost': []}
 
-    for _, row in rslt_df.iterrows():
-        if row['Schedule'] == 'mini-schedule.csv':
-            df = mini_schedule_df
-            G = mini_graph
-        else:
-            df = schedule_df
-            G = graph
-        connection, cost = solve_cost_funtion(
-            G, row['FromStation'], row['ToStation'], df, 'stops')
-        solutions['ProblemNo'].append(row['ProblemNo'])
-        solutions['Connection'].append(connection)
-        solutions['Cost'].append(cost)
+    cost_functions = [
+        ('stops', mini_graph, graph),
+        ('timeintrain', mini_graph, graph),
+        ('arrivaltime', expanded_mini_graph, expanded_graph),
+        ('traveltime', expanded_mini_graph, expanded_graph)
+    ]
 
-    for _, row in rslt_df_2.iterrows():
-        if row['Schedule'] == 'mini-schedule.csv':
-            df = mini_schedule_df
-            G = mini_graph
-        else:
-            df = schedule_df
-            G = graph
-        connection, cost = solve_cost_funtion(
-            G, row['FromStation'], row['ToStation'], df, 'timeintrain')
-        solutions['ProblemNo'].append(row['ProblemNo'])
-        solutions['Connection'].append(connection)
-        solutions['Cost'].append(cost)
-        
-    for _, row in rslt_df_4.iterrows():
-        if row['Schedule'] == 'mini-schedule.csv':
-            df = mini_schedule_df
-            G = expanded_mini_graph
-        else:
-            df = schedule_df
-            G = expanded_graph
-        connection, cost = solve_cost_funtion(
-            G, row['FromStation'], row['ToStation'], df, 'traveltime')
-        solutions['ProblemNo'].append(row['ProblemNo'])
-        solutions['Connection'].append(connection)
-        solutions['Cost'].append(cost)
+    # Iterate through all problems
+    for cost_function, mini_g, full_g in cost_functions:
+        rslt_df = problems_df.loc[(
+            problems_df['CostFunction'] == cost_function)]
+
+        for _, row in rslt_df.iterrows():
+            if row['Schedule'] == 'mini-schedule.csv':
+                df = mini_schedule_df
+                G = mini_g
+            else:
+                df = schedule_df
+                G = full_g
+
+            if cost_function == 'arrivaltime':
+                G = adjust_start_times(
+                    G, row['FromStation'], row['input_time'])
+                connection, cost = solve_cost_funtion(
+                    G, row['FromStation'], row['ToStation'], df, cost_function, row['input_time']
+                )
+            else:
+                connection, cost = solve_cost_funtion(
+                    G, row['FromStation'], row['ToStation'], df, cost_function
+                )
+
+            solutions['ProblemNo'].append(row['ProblemNo'])
+            solutions['Connection'].append(connection)
+            solutions['Cost'].append(cost)
 
     create_solutions_csv(solutions, 'solutions/my-example-solutions1.csv')
 
